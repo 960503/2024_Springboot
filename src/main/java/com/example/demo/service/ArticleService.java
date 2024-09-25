e52bc0b4f8c12fc8d3f13351fde67bf2c6f55711
960503 <hanewnji@gmail.com>
2024-09-25 19:37:00 +0900

    mybatis

---
+++ src/main/java/com/example/demo/service/ArticleService.java
@@ -0,0 +1,42 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.dao.ArticleDao;
+import com.example.demo.vo.Article;
+
+@Service
+public class ArticleService {
+
+	private ArticleDao articleDao;
+	
+	public ArticleService(ArticleDao articleDao) {
+		this.articleDao = articleDao;
+	}
+	
+	public void writeArticle(String title, String body) {
+		this.articleDao.writeArticle(title, body);
+	}
+	
+	public void modifyArticle(int id, String title, String body) {
+		this.articleDao.modifyArticle(id, title, body);
+	}
+	
+	public void deleteArticle(int id) {
+		this.articleDao.deleteArticle(id);
+	}
+	
+	public Article getArticleById(int id) {
+		return this.articleDao.getArticleById(id);
+	}
+
+	public List<Article> getArticles() {
+		return this.articleDao.getArticles();
+	}
+
+	public int getLastArticleId() {
+		return this.articleDao.getLastArticleId();
+	}
+}
