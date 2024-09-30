5d908bea50ac3269a3573c7c8796d009c36ef07f
960503 <hanewnji@gmail.com>
2024-09-30 18:08:54 +0900

    intellij

--- src/main/java/com/example/basic/ArticleDao.java
+++ src/main/java/com/example/basic/ArticleDao.java
@@ -1,34 +1,15 @@
 package com.example.basic;
 
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
-
-
 @Mapper
 public interface ArticleDao {
-
-
-    @Insert("""
-            INSERT INTO article2
-                SET title = #{title}
-                    , `body` = #{body}
-            """)
-    void save(String title, String body);
-
-
-    @Select("""
-			SELECT * FROM article2
-				ORDER BY id DESC
-			""")
-    public List<Article> getArticles();
-
-    @Select("""
-            SELECT * FROM article2
-                WHERE id = #{id}
-            """)
-    public Article findById(Long id);
+    void save(Article article);
+    List<Article> findAll();
+    Article findById(Long id);
+    void deleteById(Long id);
+    // void update(@Param("id") Long id, @Param("title") String title, @Param("body") String body);
+    void update(Article article);
 }
--- src/main/java/com/example/basic/TestController.java
+++ src/main/java/com/example/basic/TestController.java
@@ -15,88 +15,62 @@ public class TestController {
 
     private final ArticleDao articleDao;
 
-    @RequestMapping("/save")
+    @RequestMapping("/article/detail/{id}")
     @ResponseBody
-    public String save(String title, String body) {
-
-        articleDao.save(title, body);
-
-        return "게시물이 성공적으로 저장되었습니다";
-    }
-
-    @RequestMapping("/list")
-    @ResponseBody
-    public List<Article> List() {
-
-        List<Article> articleList = articleDao.getArticles();
-
-        return articleList;
-    }
-
-    @RequestMapping("/findById")
-    @ResponseBody
-    public Article findById(Long id) {
-
+    public Article detail(@PathVariable("id") long id) {
         Article article = articleDao.findById(id);
 
         return article;
     }
-    //Path Variable 방식
-    @RequestMapping("/findById1/{id}")
-    //http://localhost:8081/findById1/3
+
+    @RequestMapping("/article/list")
     @ResponseBody
-    public Article findById1(@PathVariable("id") Long id) {
-                            //url 이름과 같아야함
-        Article article1 = articleDao.findById(id);
-
-        return article1;
-    }
-
-    @RequestMapping("/t1")
-    @ResponseBody
-    public String test() {
-
-        return "test";
-    }
-
-    @RequestMapping("/t2")
-    @ResponseBody
-    public Integer t2(Integer num) {
-
-        return num * 2;
-    }
-
-    @RequestMapping("/t3")
-    @ResponseBody
-    public Article t3() {
-        Article article = new Article();
-        article.setId(1L);
-        article.setTitle("title1");
-        article.setBody("body1");
-
-        return article;
-    }
-
-    @RequestMapping("/t4")
-    @ResponseBody
-    public List<Article> t4() {
-        Article article = new Article();
-        article.setId(1L);
-        article.setTitle("title1");
-        article.setBody("body1");
-
-        Article article2 = new Article();
-        article2.setId(2L);
-        article2.setTitle("title2");
-        article2.setBody("body2");
-
-        List<Article> articleList = new ArrayList<>() {{
-            add(article);
-            add(article2);
-        }};
-
+    public List<Article> list() {
+        List<Article> articleList = articleDao.findAll();
         return articleList;
     }
 
+    @RequestMapping("/article/write")
+    @ResponseBody
+    public String write(String title, String body) {
 
+        // 코드 정리 단축키 -> 컨트롤 + 알트 + L
+        Article article = Article.builder()
+                .title(title)
+                .body(body)
+                .build();
+
+        articleDao.save(article);
+
+        return "게시물이 성공적으로 저장되었습니다";
+    }
+
+    @RequestMapping("/article/delete/{id}")
+    @ResponseBody
+    public String delete(@PathVariable long id) {
+        articleDao.deleteById(id);
+
+        return "게시물이 성공적으로 삭제되었습니다.";
+    }
+
+    @RequestMapping("/article/modify/{id}")
+    @ResponseBody
+    public String update(@PathVariable("id") long id, String title, String body) {
+
+        // 빌더 방식
+        Article article = Article.builder()
+                .id(id)
+                .title(title)
+                .body(body)
+                .build();
+
+        articleDao.update(article);
+
+        return "게시물이 성공적으로 수정되었습니다"; // 브라우저 출력 => html 문자열로 출력
+    }
+
+    @RequestMapping("/show-html")
+    public String showHtml() {
+        return "test"; // .html 확장자를 스프링부트가 자동으로 붙여줌
+    }
 }
