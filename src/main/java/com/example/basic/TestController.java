4091fc6c4b4f0b24d2905f05c88bb298ad5d8d34
960503 <hanewnji@gmail.com>
2024-09-30 20:52:58 +0900

    타임리프 조건문

--- src/main/java/com/example/basic/TestController.java
+++ src/main/java/com/example/basic/TestController.java
@@ -1,14 +1,19 @@
 package com.example.basic;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
 
-    @RequestMapping("/test")
+    @GetMapping("/variable")
     public String test(){
         return "test/variable";
+    }
+
+    @GetMapping("/condition")
+    public String condition(){
+        return "test/condition";
     }
 
 }
